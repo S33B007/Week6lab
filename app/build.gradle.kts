7cfd353f96a7c080a3592b8edd4819e234e43c58
Naseeb Oluwafisiayomi Bello <bellonas@msu.edu>
2025-07-09 19:59:18 -0500

    Step 1: Updating permissions and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
