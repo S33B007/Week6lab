33de708bbeec7e4eb7276b7bdf55f0efc52118e8
Naseeb Oluwafisiayomi Bello <bellonas@msu.edu>
2025-07-09 20:12:51 -0500

    Step 2: Make the API calls

--- app/src/main/java/com/example/randompet/MainActivity.kt
+++ app/src/main/java/com/example/randompet/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.randompet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fetchDogImage()
     }
+
+    // function to get an image URL from the Dog API
+    private fun fetchDogImage() {
+        val client = AsyncHttpClient()
+
+        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+
+            // executes if api call is successful
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful")
+            }
+
+            // if all fails
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+
+
 }
